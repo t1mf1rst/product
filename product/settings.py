8c1a363c691f8a11e001b0dc71253a8d0acbac00
Tim First <timfirst0990@gmail.com>
2018-06-10 14:51:49 +0600

    logo added

--- product/settings.py
+++ product/settings.py
@@ -123,4 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    'product/static/',
+]
+
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
